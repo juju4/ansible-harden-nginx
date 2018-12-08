5841ac93ced3f5af7f81570eeef638faee076bfe
juju4 <juju4@users.noreply.github.com>
2018-12-08 06:34:52 -0500

    galaxy lint

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -10,7 +10,7 @@ rolesdir=$(dirname $0)/..
 [ ! -d $rolesdir/juju4.w3af ] && git clone https://github.com/juju4/ansible-w3af $rolesdir/juju4.w3af
 ## galaxy naming: kitchen fails to transfer symlink folder
 #[ ! -e $rolesdir/juju4.harden-nginx ] && ln -s ansible-harden-nginx $rolesdir/juju4.harden-nginx
-[ ! -e $rolesdir/juju4.harden-nginx ] && cp -R $rolesdir/ansible-harden-nginx $rolesdir/juju4.harden-nginx
+[ ! -e $rolesdir/juju4.harden_nginx ] && cp -R $rolesdir/ansible-harden-nginx $rolesdir/juju4.harden_nginx
 
 ## don't stop build on this script return code
 true
